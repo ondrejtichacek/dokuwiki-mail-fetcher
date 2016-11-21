9365f7ea87925ae59d0fc1d7ece317a10da7f499
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-11-21 20:04:14 +0100

    Use open3.capture3 instead of `#{command}` - to properly capture both stdout and stderr.
    Dokuwiki dwpage.php probably outputs to stderr wich is not good when
    using cron to run the script.

--- fetcher.rb
+++ fetcher.rb
@@ -3,16 +3,19 @@ require 'mail'
 require 'yaml'
 require 'time'
 require 'tempfile'
+require 'open3'
 
 def repeat_try_shell(command={})
     maxtries = 14
     tries ||= maxtries
 
-    output=`#{command}`
-    result=$?.success?
+    stdout_str, stderr_str, status = Open3.capture3(command)
 
-    if result
+    if status
     else
+        puts Time.now.utc.iso8601
+        printf(stdout_str)
+        printf(stderr_str)
         numsec = 2**(maxtries - tries)
         printf(" ... will retry in %d seconds\n", numsec)
         sleep(numsec)
@@ -23,8 +26,6 @@ rescue Exception => e
     raise e
 else
 end
-
-puts Time.now.utc.iso8601
 
 #Retrieve settings from external file
 settings = YAML.load_file('settings.yml')
@@ -61,7 +62,7 @@ imap.search(["NOT", "SEEN"]).each do |message_id|
 
     #Work in temporary file
     temp_file = Tempfile.new('start')
-   
+
     begin
         #Read target file line by line, write each line to temp file and add line if condition met
         File.readlines(file_path).each do |line|
@@ -69,7 +70,7 @@ imap.search(["NOT", "SEEN"]).each do |message_id|
             temp_file.puts(line_to_add) if line.chomp == line_to_find
         end
         temp_file.close
-      
+
         #Start commiting changes
         repeat_try_shell('../bin/dwpage.php --user automat commit --message "auto update" ' + temp_file.path + ' start')
         repeat_try_shell('../bin/dwpage.php --user automat unlock start')
@@ -78,27 +79,27 @@ imap.search(["NOT", "SEEN"]).each do |message_id|
         temp_file.delete
     end
 
-    subject = mail.subject 
+    subject = mail.subject
     plain_part = mail.multipart? ? (mail.text_part ? mail.text_part.body.decoded : nil) : mail.body.decoded
     html_part = mail.html_part ? mail.html_part.body.decoded : nil
 
 #    puts plain_part
-  
+
     subject.gsub!("sometext","")
     plain_part.gsub!("othertext","")
 
-    #Parse the email to wiki syntax 
+    #Parse the email to wiki syntax
     parsed_mail = "<WRAP box 100%>\n" + \
                   "<fs 85%><color #AAAAAA>" +  mail.date.to_s + " from " + mail.from.to_s + "</color></fs>\n" + \
                   "==== " + subject + " ====\n\n" + plain_part + "</WRAP>\n"
-    
+
     #Work in temporary file
     temp_file = Tempfile.new(date)
 
     begin
         temp_file.puts(parsed_mail)
         temp_file.close
-      
+
         #Start commiting changes
         repeat_try_shell('../bin/dwpage.php --user automat commit --message "auto update" ' + temp_file.path + ' mail:' + date)
 
@@ -106,10 +107,10 @@ imap.search(["NOT", "SEEN"]).each do |message_id|
         temp_file.delete
     end
 
+    puts Time.now.utc.iso8601
     puts "One message treated"
 
 end
 
 imap.expunge #Delete all mails with deleted flags
 imap.close
-
